5088e21ff8cef708d45ce946039c68e10b540689
kaika81 <gmizrahi81@gmail.com>
2026-04-16 11:46:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,25 @@
 const CACHE_NAME = 'recipes-app';
 
 self.addEventListener('install', () => {
-self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-event.waitUntil(
-caches.keys().then((keys) =>
-Promise.all(keys.map((key) => caches.delete(key)))
-).then(() => self.clients.claim())
-);
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-event.respondWith(
-fetch(event.request)
-.then((response) => {
-const responseClone = response.clone();
-caches.open(CACHE_NAME).then((cache) => {
-cache.put(event.request, responseClone);
-});
-return response;
-})
-.catch(() => caches.match(event.request))
-);
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
