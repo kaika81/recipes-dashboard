76e22373b6254ba6c6404b39a1a16721b4c5ec99
kaika81 <gmizrahi81@gmail.com>
2026-05-01 18:17:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ const shoppingInput = document.getElementById("shoppingInput");
 const addShoppingItemBtn = document.getElementById("addShoppingItem");
 const shoppingList = document.getElementById("shoppingList");
 
-const SHOPPING_API_URL = "https://script.google.com/macros/s/AKfycbzJ4koLQ0XOjNr6fUl_T_CFgcTqnnWU4cCqnZLQjvqOYY9LABJJrl2IB3G6cujfWPhs/exec";
+const SHOPPING_API_URL = "https://script.google.com/macros/library/d/1hJk_RbJl-DFNpUjyzggfXwdl64W6uwAT2jeWN7ZhuGAn5ekMGzPMu0xL/2";
 
 let currentCategory = null;
 let pathStack = [];
